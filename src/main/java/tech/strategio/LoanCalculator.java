45cc8e8663f36abc79b4b1168b38a2eaa9be5593
Mimi Meyer <62210528+MimiMeyer@users.noreply.github.com>
2022-10-24 16:41:21 -0400

    updating the java doc and the method

--- src/main/java/tech/strategio/LoanCalculator.java
+++ src/main/java/tech/strategio/LoanCalculator.java
@@ -6,14 +6,22 @@ public class LoanCalculator {
 
     /**
      * TODO: finish out this JavaDoc comment block.
-     * FIXME: WHAT DOES THIS METHOD DO?
      * 
-     * @param ???
-     * @return ???
+     * Calculates the amount earned after 3 months 
+     * if the loaner payed evry month 10% of the amount owed.
+     * @param the full amount owned (int)
+     * @return the amount owned after 3 months (int)
      */
     static int getRemainingAmountIn3Months(int amount) {
         // TODO: Rewrite this method
-        return -1;
+        int month1,month2,month3,newAmount;
+		month1 = (int)(amount*0.1);
+		newAmount = amount - month1;
+		month2 = (int)(newAmount*0.1);
+		newAmount-=month2;
+		month3 =(int)(newAmount*0.1);
+		newAmount-=month3;
+        return newAmount;
     }
 
     public static void main(String[] args) {
